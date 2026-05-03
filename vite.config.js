32913bc95ded6af27fde8d7e0d5d84b504987fb0
alfauzan003 <habibalfauzan1@gmail.com>
2026-05-03 10:50:56 +0700

    Fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/personal-web/',
+  base: process.env.NODE_ENV === 'production' ? '/personal-web/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
